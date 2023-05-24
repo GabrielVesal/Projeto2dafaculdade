579147234ed2ef3a30472ecc9bd6ef088cc72324
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-05-24 19:42:51 -0300

    Update .py

--- .py
+++ .py
@@ -1,3 +1,15 @@
+'''
+input velocidade
+velocidade_maxima = 80
+if velocidade <= velocidade_maxima:
+print "Não levou multa" 
+if velocidade > velocidade_maxima e velocidade <= velocidade_maxima + 10:
+print "Levou multa leve"
+if velocidade > velocidade_maxima +11 e velocidade <= velocidade_maxima + 20:
+print "Levou multa grave"
+if velocidade > velocidade_maxima + 20:
+print "Levou multa gravíssima"
+'''
 
 velocidade = int(input('Digite sua velocidade: '))
 velocidade_maxima = 80
